cec1956d46b07a760278544c5dcbd979c3900622
Takashi Iwai <tiwai@suse.de>
2016-02-16 14:15:59 +0100

    ALSA: seq: Fix double port list deletion

--- sound/core/seq/seq_ports.c
+++ sound/core/seq/seq_ports.c
@@ -527,7 +527,47 @@ int snd_seq_port_connect(struct snd_seq_client *connector,
 	if ((err = subscribe_port(src_client, src_port, src, info,
 				  connector->number != src_client->number)) < 0)
 		goto __error;
-	src_called = 1;
+
+	}
+
+	/* add to list */
+	write_lock_irq(&grp->list_lock);
+	if (is_src)
+		list_add_tail(&subs->src_list, &grp->list_head);
+	else
+		list_add_tail(&subs->dest_list, &grp->list_head);
+	grp->exclusive = exclusive;
+	atomic_inc(&subs->ref_count);
+	write_unlock_irq(&grp->list_lock);
+	err = 0;
+
+ __error:
+	up_write(&grp->list_mutex);
+	return err;
+}
+
+static void delete_and_unsubscribe_port(struct snd_seq_client *client,
+					struct snd_seq_client_port *port,
+					struct snd_seq_subscribers *subs,
+					bool is_src, bool ack)
+{
+	struct snd_seq_port_subs_info *grp;
+	struct list_head *list;
+	bool empty;
+
+	grp = is_src ? &port->c_src : &port->c_dest;
+	list = is_src ? &subs->src_list : &subs->dest_list;
+	down_write(&grp->list_mutex);
+	write_lock_irq(&grp->list_lock);
+	empty = list_empty(list);
+	if (!empty)
+		list_del_init(list);
+	grp->exclusive = 0;
+	write_unlock_irq(&grp->list_lock);
+	up_write(&grp->list_mutex);
+
+	if (!empty)
+		unsubscribe_port(client, port, grp, &subs->info, ack);
 
 	if ((err = subscribe_port(dest_client, dest_port, dest, info,
 				  connector->number != dest_client->number)) < 0)
