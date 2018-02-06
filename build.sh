3f6ec65833e440e134099ab74ca82986f603f5e5
petya230 <petya230@gmail.com>
2018-02-06 12:08:19 +0100

    things

---
+++ build.sh
@@ -0,0 +1,13 @@
+# make clea
+# make mrproper
+export ARCH=arm64
+export CROSS_COMPILE=../gcc/bin/aarch64-linux-android-
+export KBUILD_BUILD_USER="Petya230"
+export KBUILD_BUILD_HOST="CodeOfHonor.Tech"
+echo -e "\e[31mRemove out"
+rm -r ../out
+echo -e "\e[32mMake out"
+mkdir ../out
+echo -e "\e[97mBuil"
+make ARCH=arm64 O=../out merge_hi6250_defconfig
+time make ARCH=arm64 O=../out -j8 |& tee szar
