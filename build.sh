5a0e23ab35c0acecda6d8740485f3030fe103d85
petya230 <petya230@gmail.com>
2018-02-15 11:39:28 +0100

    meehhhhh

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 # make mrproper
 export ARCH=arm64
 export CROSS_COMPILE=../gcc/bin/aarch64-linux-android-
-export KBUILD_BUILD_USER="IMPEACH TRUMP!"
+export KBUILD_BUILD_USER="IMPEACH_TRUMP!"
 export KBUILD_BUILD_HOST="CodeOfHonor.Tech"
 echo -e "\e[31mRemove out"
 rm -r ../out
 echo -e "\e[32mMake out"
 mkdir ../out
-echo -e "\e[97mBuil"
+echo -e "\e[97mBuild"
 make ARCH=arm64 O=../out merge_hi6250_defconfig
 time make ARCH=arm64 O=../out -j8 |& tee szar
